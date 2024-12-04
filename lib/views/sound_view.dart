4ebb83bfb94cddecb007039fcd162ca61b816739
hedqu033 <hedqu033@d.umn.edu>
2024-12-04 15:45:22 -0600

    first commit

---
+++ lib/views/sound_view.dart
@@ -0,0 +1,77 @@
+//import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+void main() => runApp(MP3PlayerApp());
+
+class MP3PlayerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MP3 Player',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MP3PlayerScreen(),
+    );
+  }
+}
+
+class MP3PlayerScreen extends StatefulWidget {
+  @override
+  _MP3PlayerScreenState createState() => _MP3PlayerScreenState();
+}
+
+class _MP3PlayerScreenState extends State<MP3PlayerScreen> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playPause() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      // Replace with the URL or file path of your MP3
+      const audioUrl = 'https://www.example.com/sample.mp3';
+      try {
+        await _audioPlayer.setUrl(audioUrl);
+        await _audioPlayer.play();
+      } catch (e) {
+        print('Error loading audio: $e');
+      }
+    }
+
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MP3 Player'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              _isPlaying ? Icons.pause_circle_filled : Icons.play_circle_filled,
+              size: 100,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _playPause,
+              child: Text(_isPlaying ? 'Pause' : 'Play'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
